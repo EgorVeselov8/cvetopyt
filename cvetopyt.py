06b5a08ab2ae8837e56824b3fad1a1925e60cb63
EgorVeselov8 <151117355+EgorVeselov8@users.noreply.github.com>
2023-11-18 17:18:47 +0300

    Update cvetopyt.py

--- cvetopyt.py
+++ cvetopyt.py
@@ -1,92 +1,88 @@
-from flask import Flask, request, Response
 import requests
 from bs4 import BeautifulSoup
 import re
 from urllib.parse import urlparse, urljoin
 from collections import Counter
-import json
 
-app = Flask(__name__)
 
 def standardize_color(color):
-    if len(color) == 4:  
+    if len(color) == 4:  # One additional character for "#"
         return "#" + color[1] * 2 + color[2] * 2 + color[3] * 2
     return color.lower()
+
 
 def is_valid_url(url):
     parsed = urlparse(url)
     return bool(parsed.netloc) and bool(parsed.scheme)
 
-def fetch_html_content(url, timeout=10):
-    try:
-        response = requests.get(url, timeout=timeout)
-        response.raise_for_status()
-    except (requests.exceptions.Timeout, requests.exceptions.TooManyRedirects, requests.exceptions.HTTPError) as e:
-        print(f"An error occurred when accessing the website {url}: {str(e)}")
-        return None
-    except requests.exceptions.RequestException as e:
-        print(f"An unexpected error occurred when accessing the website {url}: {str(e)}")
-        return None
-    else:
-        return response.text
 
 def get_all_website_links(url):
     urls = set()
     domain_name = urlparse(url).netloc
-    html_content = fetch_html_content(url)
-    if not html_content:
-        return urls
 
-    soup = BeautifulSoup(html_content, "html.parser")
+    soup = BeautifulSoup(requests.get(url).content, "html.parser")
+
     for a_tag in soup.findAll("a"):
         href = a_tag.attrs.get("href")
         if href == "" or href is None:
             continue
+
         href = urljoin(url, href)
         parsed_href = urlparse(href)
+
         if parsed_href.netloc != domain_name:
             continue
 
         urls.add(href)
     return urls
 
+
 def find_colors_on_website(url):
-    pattern = r'#(?:[0-9a-fA-F]{3}){1,2}|rgba?(\s*\d{1,3}\s*,\s*\d{1,3}\s*,\s*\d{1,3}\s*,\s*(?:1|0?.\d+?)\s*)'
-    all_website_links = get_all_website_links(url)
-    all_website_links.add(url)
     colors = []
+    all_website_links = get_all_website_links(url)
 
     for url in all_website_links:
-        html_content = fetch_html_content(url)
-        if not html_content:
-            continue
+        try:
+            # pattern for hex and rgba colors
+            pattern = r'#(?:[0-9a-fA-F]{3}){1,2}|rgba?\(\s*\d{1,3}\s*,\s*\d{1,3}\s*,\s*\d{1,3}\s*,\s*(?:1|0?\.\d+?)\s*\)'
 
-        soup = BeautifulSoup(html_content, "html.parser")
-        link_tags = soup.find_all('link', rel='stylesheet')
-        css_urls = [urljoin(url, tag.get('href')) for tag in link_tags]
+            response = requests.get(url)
+            response.raise_for_status()
 
-        style_tags = soup.find_all('style')
-        styles = " ".join(tag.get_text(" ", strip=True) for tag in style_tags)
-        colors.extend(map(standardize_color, re.findall(pattern, styles)))
+            soup = BeautifulSoup(response.text, 'html.parser')
 
-        for css_url in css_urls:
-            css_content = fetch_html_content(css_url)
-            if not css_content:
-                continue
-            colors.extend(map(standardize_color, re.findall(pattern, css_content)))
+            link_tags = soup.find_all('link', rel='stylesheet')
 
+            css_urls = [urljoin(url, tag.get('href')) for tag in link_tags]
+
+            style_tags = soup.find_all('style')
+
+            styles = " ".join(tag.get_text(" ", strip=True) for tag in style_tags)
+
+            # Extract all color codes
+            colors.extend(map(standardize_color, re.findall(pattern, styles)))
+
+            for css_url in css_urls:
+                css_response = requests.get(css_url)
+                css_response.raise_for_status()
+                css_text = css_response.text
+
+                # find colors in the external CSS
+                colors.extend(map(standardize_color, re.findall(pattern, css_text)))
+
+        except requests.exceptions.RequestException as e:
+            print("An error occurred when accessing the website:", e)
+
+    # Count usage and sort color list
     color_counts = dict(Counter(colors))
     sorted_colors = sorted(color_counts.items(), key=lambda item: item[1], reverse=True)
+
     return sorted_colors
 
-@app.route("/colors", methods=["GET"])
-def get_website_colors():
-    website_url = request.args.get("url")
-    if website_url and is_valid_url(website_url):
-        colors = find_colors_on_website(website_url)
-        return Response(json.dumps(colors), mimetype='application/json')
-    else:
-        return Response(json.dumps({"error": "Invalid url"}), mimetype='application/json', status=400)
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+website_url = "https://global.wildberries.ru"
+colors = find_colors_on_website(website_url)
+
+print("List of found colors sorted by usage:")
+for color, count in colors:
+    print(f"{color}: {count}")
